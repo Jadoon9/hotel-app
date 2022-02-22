3fb6f9101cb1217e3279ad701a0cad643e4a4ac9
MrAnAcondA <shahzebjadoon0@gmail.com>
2022-02-23 01:05:50 +0500

    setup basic folder structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div className='App'></div>;
-}
+import Navbar from './components/Navbar/Navbar';
+import Bookings from './components/Bookings/Bookings';
+import Hero from './components/Hero/Hero';
+import AboutUs from './components/AboutUs/AboutUs';
+import Services from './components/Services/Services';
+import WhyUs from './components/WhyUs/WhyUs';
+import ContactUs from './components/ContactUs/ContactUs';
+import Footer from './components/Footer/Footer';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Bookings />
+      <Hero />
+      <AboutUs />
+      <Services />
+      <WhyUs />
+      <ContactUs />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
